4fc0be6bef0a1f7449b980baa84bcd935507f873
wangqi <wang.qi@qikuyx.com>
2025-04-29 10:55:06 -0400

    Add .unsafeFlags([-Xlinker, -no_code_signature_info]) to target JSONSchema

--- Package.swift
+++ Package.swift
@@ -38,6 +38,9 @@ let package = Package(
       name: "JSONSchema",
       resources: [
         .process("Resources")
+      ],
+      linkerSettings: [
+        .unsafeFlags(["-Xlinker", "-no_code_signature_info"])
       ]
     ),
     .testTarget(
